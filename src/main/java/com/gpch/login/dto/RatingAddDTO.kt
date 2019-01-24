bd9a78829f48f89dae3706e958e92c4999c5444e
Krzysztof Kutyła <krzysztof.kutyla1997@gmail.com>
2019-01-24 02:56:50 +0100

    lista moich eventow na main page, proby rating v2

--- src/main/java/com/gpch/login/dto/RatingAddDTO.kt
+++ src/main/java/com/gpch/login/dto/RatingAddDTO.kt
@@ -5,11 +5,13 @@ import com.gpch.login.model.Place
 import com.gpch.login.model.User
 import org.springframework.stereotype.Component
 import java.util.*
+import javax.persistence.Column
 import javax.validation.constraints.NotBlank
 
 @Component
 data class  RatingAddDTO constructor(
-        var event : Event? = null,
-        var score: Int? = null
+
+        var score: Int? = null,
+        var user: User? = null
 
 )
